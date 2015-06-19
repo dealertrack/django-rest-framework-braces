0a7a0deb22c452587be962ed50179966bc5bdb6c
Khaled Porlin <Khaled.Porlin@dealertrack.com>
2015-06-19 10:53:16 -0700

    Added unit test for SortedJSONParser

---
+++ drf_braces/tests/test_parsers.py
@@ -0,0 +1,24 @@
+from __future__ import print_function, unicode_literals
+import json
+import unittest
+from collections import OrderedDict
+
+import six
+
+from drf_braces.parsers import SortedJSONParser
+
+
+class TestSortedJSONParser(unittest.TestCase):
+
+    def setUp(self):
+        super(TestSortedJSONParser, self).setUp()
+        self.parser_class = SortedJSONParser()
+
+    def test_sorted_json_parser(self):
+        content = json.dumps({"hello": "world"})
+        stream = six.BytesIO(content)
+        expected_data = OrderedDict([('hello', 'world')])
+
+        actual_data = self.parser_class.parse(stream=stream)
+
+        self.assertEqual(expected_data, actual_data)
