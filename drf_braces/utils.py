227b7284c2f6b679e56c1610dc2f82f7f45c0f4a
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2015-07-13 11:22:09 -0400

    bumped version to 0.1.4

--- drf_braces/utils.py
+++ drf_braces/utils.py
@@ -174,7 +174,7 @@ def get_attr_from_base_classes(bases, attrs, attr, default=None):
         except AttributeError:
             continue
 
-    if default:
+    if default is not None:
         return default
 
     raise AttributeError(
