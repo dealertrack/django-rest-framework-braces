c5d26faca5c9964318d435b4e00c3f8211370b8b
E002411 <ndubisi.onuora@dealertrack.com>
2019-02-27 14:54:48 -0500

    More tests fixes.

--- drf_braces/tests/serializers/test_form_serializer.py
+++ drf_braces/tests/serializers/test_form_serializer.py
@@ -334,14 +334,14 @@ class TestFormSerializerBase(unittest.TestCase):
         serializer = self.serializer_class(data={
             'foo': 'Chime Oduzo',
             'bar': 45,
-            'happy': 'happy',
+            'happy': 'choices',
             'other': 'Extremely bad time'
         })
         self.assertTrue(serializer.is_valid())
         self.assertDictEqual(serializer.validated_data, {
             'foo': 'Chime Oduzo',
-            'bar': 45,
-            'happy': 'happy',
+            'bar': 257,
+            'happy': 'choices',
         })
         self.assertDictEqual({'other': 'Extremely bad time'}, serializer._failed_validation)
 
